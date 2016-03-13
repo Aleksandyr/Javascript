f5fe0b34eb678abf52979eacb5b634e34c8df498
Aleksandyr <aleksandyr.kamenov@gmail.com>
2016-03-13 23:42:31 +0200

    all my work with javascript

---
+++ JavascriptApplication/04.BaaSAndParseHomework/02, 03, 04, 05/data.js
@@ -0,0 +1,116 @@
+'use strict';
+
+var app = app || {};
+
+(function(scope){
+    scope.data = {};
+    var BASE_URL = 'http://baas.kinvey.com';
+    var PARSE_APP_ID = 'kid_ZJNYs_CZkZ';
+    var PARSE_REST_API_KEY = '4760c22bf68040c18f49e3f60380672d';
+
+    var CRUD = function(crudMethod, crudUrl, crudData, crudSuccessCallback, crudErrorCallback, contentType){
+        var USER_AUTH = 'cGVzaG86MTIzNDU2';
+
+        $.ajax({
+            method: crudMethod,
+            headers: {
+                'Authorization' : 'Basic ' + USER_AUTH,
+                'X-Kinvey-API-Version' : '3',
+                'Content-Type' : contentType
+            },
+            url: crudUrl,
+            data: JSON.stringify(crudData)
+        }).success(crudSuccessCallback).error(crudErrorCallback);
+    };
+
+    scope.loadCountries = function(){
+        var allPath = BASE_URL + '/appdata/' + PARSE_APP_ID + '/countries';
+        CRUD('GET', allPath, null, scope.successfulCountriesLoad, scope.showAJAXError, 'application/json');
+    };
+
+    scope.loadCountryTowns = function(e){
+        var countryId = $(this).parent().attr('data-id');
+        var query = '?query={"country._id":"' + countryId + '"}';
+        var getCountryTownsUrl = BASE_URL + '/appdata/' + PARSE_APP_ID + '/towns' + query;
+
+        scope.data.countryId = countryId;
+        CRUD('GET', getCountryTownsUrl, null, scope.successfulTownsLoad, scope.showAJAXError);
+
+        e.preventDefault();
+    };
+
+    scope.editCountry = function(e){
+        var countryId = $('#edit-country').attr('data');
+        var country = $('#edit-country-value').val();
+        var editData = {'name': country};
+
+        CRUD('PUT', BASE_URL + '/appdata/' + PARSE_APP_ID + '/countries/' + countryId, editData, scope.reloadCountries, scope.showAJAXError, 'application/json');
+
+        e.preventDefault();
+    };
+
+    scope.editTown = function(e){
+        var townId = $('#edit-town-value').attr('data');
+        var town = $('#edit-town-value').val();
+        //var editData = {'name' : town};
+        var countryId = $('#new-town-value').attr('data');
+        var newTownData = {
+            'country': {
+                "__type": "KinveyRef",
+                "_collection": "countries",
+                "_id": countryId
+            },
+            'name': town
+        };
+
+
+        CRUD('PUT', BASE_URL + '/appdata/' + PARSE_APP_ID + '/towns/' + townId, newTownData, scope.reloadCountries, scope.showAJAXError, 'application/json');
+
+        e.preventDefault();
+    };
+
+    scope.addNewCountry = function(e){
+        var newCountryValue = $('#add-country').val();
+        var newCountryData = {'name': newCountryValue};
+
+        var allPath = BASE_URL + '/appdata/' + PARSE_APP_ID + '/countries';
+        CRUD('POST', allPath, newCountryData, scope.reloadCountries, scope.showAJAXError, 'application/json');
+
+        $('#add-country').val('');
+        e.preventDefault();
+    };
+
+    scope.addNewTown = function(e){
+        var newTownValue = $('#new-town-value').val();
+        var countryId = $('#new-town-value').attr('data');
+        var newTownData = {
+            'country': {
+                "__type": "KinveyRef",
+                "_collection": "countries",
+                "_id": countryId
+            },
+            'name': newTownValue
+        };
+
+        var allPath = BASE_URL + '/appdata/' + PARSE_APP_ID + '/towns';
+        CRUD('POST', allPath, newTownData, scope.reloadCountries, scope.showAJAXError, 'application/json');
+
+        e.preventDefault();
+    };
+
+    scope.deleteCountry = function(e){
+        var countryId = $(this).parent().attr('data-id');
+
+        CRUD('DELETE', BASE_URL + '/appdata/' + PARSE_APP_ID + '/countries/' + countryId, null, scope.reloadCountries, scope.showAJAXError);
+
+        e.preventDefault();
+    };
+
+    scope.deleteTown = function(e){
+        var townId = $(this).parent().attr('data-id');
+
+        CRUD('DELETE', BASE_URL + '/appdata/' + PARSE_APP_ID + '/towns/' + townId, null, scope.reloadCountries, scope.showAJAXError);
+
+        e.preventDefault();
+    };
+}(app));
---
+++ JavascriptApplication/05.ConsumingRemoteData/app.js
@@ -0,0 +1,184 @@
+'use strict';
+
+var app = app || {};
+
+(function(scope){
+    var APP_ID, RESOURCE_URL, USER_AUTH;
+
+    var listAllBooks,
+        errorMsg, successMsg,
+        showEditForm, deleteBook;
+
+    RESOURCE_URL = 'http://baas.kinvey.com';
+    APP_ID = 'kid_-ywUA-g7k-';
+    USER_AUTH = 'cGVzaG86MTIzNA==';
+
+    successMsg = $('.messages .success');
+    errorMsg = $('.messages .error');
+
+    $(function(){
+        listAllBooks();
+    });
+
+    listAllBooks = function(){
+        return $.ajax({
+            headers: {
+                'Authorization' : 'Basic ' + USER_AUTH
+            },
+            url: RESOURCE_URL + '/appdata/' + APP_ID + '/books',
+            type: 'GET',
+            contentType: 'application/json',
+            success: function(data){
+                successMsg
+                    .html('', data.name + ' successfully listed')
+                    .show()
+                    .fadeOut(2000);
+
+                $('#books').text('');
+
+                for(var b in data){
+                    var book = data[b];
+                    var bookRow = $('<tr>');
+                    bookRow.append($('<td id="title">').text(book.title));
+                    bookRow.append($('<td id="author">').text(book.author));
+                    bookRow.append($('<td id="isbn">').text(book.isbn));
+                    bookRow.append($('<td>').html($('<a href="#" data-id="' + book._id + '">').text('Edit').on('click', showEditForm)));
+                    bookRow.append($('<td>').html($('<a href="#" data-id="' + book._id + '">').text('Delete').on('click', deleteBook)));
+                    bookRow.appendTo($('#books'));
+                }
+            },
+            error: function(err){
+                errorMsg
+                    .html('Error happened: ' + err)
+                    .show()
+                    .fadeOut(2000);
+            }
+        });
+    };
+
+    deleteBook = function(e){
+        var bookId = $(this).attr('data-id');
+
+        $.ajax({
+            headers: {
+                'Authorization' : 'Basic ' + USER_AUTH
+            },
+            url: RESOURCE_URL + '/appdata/' + APP_ID + '/books/' + bookId,
+            type: 'DELETE',
+            success: function(data){
+                successMsg.html('' + data.name + ' successfully deleted')
+                    .show()
+                    .fadeOut(2000);
+                listAllBooks();
+            },
+            error: function(err){
+                errorMsg
+                    .html('Error happened: ' + err)
+                    .show()
+                    .fadeOut(2000);
+            }
+        });
+
+        e.preventDefault();
+    };
+
+    showEditForm = function(){
+        var currentRow = $(this).parent().parent();
+        var bookTitle = currentRow.find('#title').text();
+        var bookAuthor = currentRow.find('#author').text();
+        var bookIsbn = currentRow.find('#isbn').text();
+        var bookId = $(this).attr('data-id');
+
+        $('#book-title').val(bookTitle);
+        $('#book-author').val(bookAuthor);
+        $('#book-isbn').val(bookIsbn);
+        $('#book-id').val(bookId);
+        $('#book-form').fadeIn();
+    };
+
+    $(function(){
+        $('#book-edit').on('click', function(e){
+            var bookId = $('#book-id').val();
+            var bookTitle = $('#book-title').val();
+            var bookAuthor = $('#book-author').val();
+            var bookIsbn = $('#book-isbn').val();
+
+            var bookData = {
+                'title' : bookTitle,
+                'author' : bookAuthor,
+                'isbn' : bookIsbn
+            };
+
+            $.ajax({
+                method: 'PUT',
+                headers: {
+                    'Authorization' : 'Basic ' + USER_AUTH,
+                    'Content-Type' : 'application/json'
+                },
+
+                url: RESOURCE_URL + '/appdata/' + APP_ID + '/books/' + bookId,
+                data: JSON.stringify(bookData),
+                success: function(data){
+                    successMsg.html('' + data.name + ' successfully edited')
+                        .show()
+                        .fadeOut(2000);
+                    $('#book-form').fadeOut();
+                    $('#books').text('');
+                    listAllBooks();
+                },
+                error: function(err){
+                    errorMsg
+                        .html('Error happened: ' + err)
+                        .show()
+                        .fadeOut(2000);
+                }
+            });
+
+            e.preventDefault();
+        });
+    });
+
+
+    $(function(){
+        $('#book-create').on('click', function(e){
+            var bookTitle = $('#new-book-title').val();
+            var bookAuthor = $('#new-book-author').val();
+            var bookIsbn = $('#new-book-isbn').val();
+
+            var bookData = {
+                'title' : bookTitle,
+                'author' : bookAuthor,
+                'isbn' : bookIsbn
+            };
+
+            $.ajax({
+                method: 'POST',
+                headers: {
+                    'Authorization' : 'Basic ' + USER_AUTH,
+                    'Content-Type' : 'application/json'
+                },
+
+                url: RESOURCE_URL + '/appdata/' + APP_ID + '/books',
+                data: JSON.stringify(bookData),
+                success: function(data){
+                    successMsg.html('' + data.name + ' successfully created')
+                        .show()
+                        .fadeOut(2000);
+
+                    $('#new-book-title').val('');
+                    $('#new-book-author').val('');
+                    $('#new-book-isbn').val('');
+                    listAllBooks();
+                },
+                error: function(err){
+                    errorMsg
+                        .html('Error happened: ' + err)
+                        .show()
+                        .fadeOut(2000);
+                }
+            });
+
+            e.preventDefault();
+        });
+    });
+}(app));
